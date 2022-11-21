ecb99c6e97cd0d030ba5d572e8ba200ce76225d4
Arnab Banik <84622873+ArnabBanik-repo@users.noreply.github.com>
2022-11-21 10:46:42 +0530

    Renaming fixed

--- renamer.py
+++ renamer.py
@@ -2,27 +2,39 @@ import os
 import re
 import platform
 import random
+from zipfile import ZipFile
 # dir = os.path.dirname(__file__)
 usr = os.getlogin()
 basedir = '/home/'+usr+'/Downloads/'
 if platform.system() == "Windows":
-    basedir = 'C:/Downloads/'
+    basedir = 'C:/Users/'+usr+'/Downloads/'
 
 for dir in os.listdir(basedir):
-    org = os.path.join(basedir, dir)
-    if not os.path.isdir(os.path.join(basedir, dir)):
-        continue
     if not re.match("CoursePage-Material_", dir):
         continue
-    else:
+    if not os.path.isdir(os.path.join(basedir, dir)):
+        if os.path.splitext(dir)[1] == '.zip':
+            with ZipFile(os.path.join(basedir, dir), 'r') as zObject:
+                zObject.extractall(path=os.path.join(
+                    basedir, os.path.splitext(dir)[0]))
+            dir = os.path.splitext(dir)[0]
+            # print(dir)
+        else:
+            continue
+    if os.path.isdir(os.path.join(basedir, dir)):
+        org = os.path.join(basedir, dir)
         dir = (re.findall("\\_\w{3}\d{4}\\_", dir)[0].strip("_"))
         dir = os.path.join(basedir, dir)
         try:
             os.rename(org, dir)
         except:
-            dir = os.rename(org, dir+"_"+str.format(random.randint(1, 1000)))
+            dir = dir+"_"+str(random.randint(1, 1000))
+            os.rename(org, dir)
         files = os.listdir(os.path.join(basedir, dir))
+        k = 1
         for i in files:
+            if (os.path.isdir(os.path.join(basedir, dir, i))):
+                continue
             if i == 'renamer.py':
                 continue
             name, ext = os.path.splitext(i)
@@ -33,15 +45,25 @@ for dir in os.listdir(basedir):
                 name = re.sub("(\\_)\d*", " ", name)
                 name = re.sub("-\d+", "", name)
                 name = re.sub("\s+", " ", name)
+                name = re.sub("\s+\W", "", name)
+                name = name.capitalize()
             except:
                 try:
-                    name = (re.split("_\d", re.split("REFERENCE_MATERIAL_", name)[1])[
+                    name = (re.split("_\d", re.split("VL\d{13}", name)[1])[
                         0] + "_Syllabus")
+                    # print(name)
                 except:
-                    print("Some error")
+                    # print("Some error")
+                    name = "Syllabus"
                 # print("Not handled")
 
+            name = re.sub("\\)", "", name)
             name = name.strip(" ")
-            name += ext
-
-            os.rename(os.path.join(dir, i), os.path.join(dir, name))
+            # name += ext
+            if not os.path.exists(os.path.join(dir, name + ext)):
+                os.rename(os.path.join(dir, i), os.path.join(dir, name + ext))
+            else:
+                os.rename(os.path.join(dir, i), os.path.join(
+                    dir, name + "_" + str(k) + ext))
+                k += 1
+            # print(name+"."+ext)
